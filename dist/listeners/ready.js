f3258450ed6525444cb7c44e52ef77522de97e26
Bread <72562619+NotRealArif@users.noreply.github.com>
2022-11-30 18:23:31 +0000

    Ts complied

--- dist/listeners/ready.js
+++ dist/listeners/ready.js
@@ -4,7 +4,9 @@ exports.ready = void 0;
 var tslib_1 = require("tslib");
 var chalk_1 = (0, tslib_1.__importDefault)(require("chalk"));
 function ready(client) {
-    client.editStatus('online', { name: 'Bakery bot!', type: 0 });
-    console.log(chalk_1.default.greenBright("[Discord API] " + client.user.username + " is now connected to Discord!"));
+    client.on('ready', function () {
+        client.editStatus('online', { name: 'Bakery bot!', type: 0 });
+        console.log(chalk_1.default.greenBright("[Discord API] " + client.user.username + " is now connected to Discord!"));
+    });
 }
 exports.ready = ready;
